89c0f56e7880ae4d7312aabad8c3d3b43300021e
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2020-03-12 23:19:55 +0900

    build: improve Make validate

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -4,6 +4,8 @@ set -eu
 
 cd "$(dirname "$0")/.."
 
+circleci config pack src | circleci orb validate -
+
 for file in tests/*; do
   filepath=$(mktemp)
   if cat <(circleci config pack src) "$file" | circleci config validate - > "$filepath" 2>&1; then
